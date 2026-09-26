798a3524b5af8b5ef85bb2d33b03e81bccab6d40
Ted903 <jang.mh@sannamchon.co.kr>
2026-09-26 22:21:07 +0900

    후쿠오카 카페·디저트 148곳 추가 + 식사→카페 8곳 이동 (v29)

--- app.js
+++ app.js
@@ -1824,7 +1824,7 @@ Object.assign(window,{joinByCode,copyCode});
 
 (function boot(){
  load(); loadFXCache();
- fetch('places.json?v=7').then(r=>r.json()).then(function(d){
+ fetch('places.json?v=8').then(r=>r.json()).then(function(d){
   DB=d;
   const t=T();
   const regs=[...new Set(DB.map(p=>p.r))];
